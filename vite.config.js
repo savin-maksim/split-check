9d36d8f33dd1cf3f2d8a67f5828a100de362d374
savin-maksim <savin.m.a.25pt@gmail.com>
2024-11-03 14:52:20 +0300

    seo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,23 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          utils: ['mathjs', 'react-hot-toast']
+        }
+      }
+    }
+  },
   server: {
     host: true
   },
